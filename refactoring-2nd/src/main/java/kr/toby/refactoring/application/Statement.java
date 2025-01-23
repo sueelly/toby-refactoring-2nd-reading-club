ae798e56d17953ab38c153cc227f11dc9e612b69
김혜주 <sueelly@naver.com>
2025-01-23 17:04:23 +0900

    feat(Statement): Implements first example

--- refactoring-2nd/src/main/java/kr/toby/refactoring/application/Statement.java
+++ refactoring-2nd/src/main/java/kr/toby/refactoring/application/Statement.java
@@ -1,6 +1,58 @@
-package java.
+package kr.toby.refactoring.application;
+
+import kr.toby.refactoring.common.exception.ResultCode;
+import kr.toby.refactoring.common.exception.RefactoringException;
+import kr.toby.refactoring.domain.invoice.Invoice;
+import kr.toby.refactoring.domain.invoice.Performance;
+import kr.toby.refactoring.domain.play.Plays;
+import kr.toby.refactoring.domain.play.Play;
+import kr.toby.refactoring.domain.play.enums.Type;
 
 public class Statement {
 
+    public String statement(Invoice invoice, Plays plays) 
+            throws RefactoringException {
+        int totalAmount = 0;
+        int volumeCredit = 0;
+        StringBuilder result = new StringBuilder(
+                String.format("청구내역 (고객명: %s)\n", invoice.getCustomer()));
 
+        for (Performance performance : invoice.getPerformances()) {
+            Play play = plays.get(performance);
+            int thisAmount = 0;
+
+            switch (play.getType()) {
+                case TRAGEDY:
+                    thisAmount = 40000;
+                    if (performance.getAudience() > 30) {
+                        thisAmount += 1000 * (performance.getAudience() - 30);
+                    }
+                    break;
+                case COMEDY:
+                    thisAmount = 30000;
+                    if (performance.getAudience() > 20) {
+                        thisAmount += 10000 + 500 * (performance.getAudience() - 20);
+                    }
+                    thisAmount += 300 * performance.getAudience();
+                    break;
+                default:
+                    throw new RefactoringException(ResultCode.INVALID_PLAY_TYPE);
+            }
+            // Add volume credits for each performance
+            volumeCredit += Math.max(performance.getAudience() - 30, 0);
+
+            // Add extra volume credits for every 5 comedy attendees
+            if (play.getType() == Type.COMEDY) {
+                volumeCredit += Math.floor(performance.getAudience() / 5);
+            }
+
+            // Print invoice line for this order
+            result.append(String.format("%s: $%d %d석\n", play.getName(), thisAmount / 100,
+                    performance.getAudience()));
+            totalAmount += thisAmount;
+        }
+        result.append(String.format("총액: $%d\n", totalAmount / 100));
+        result.append(String.format("적립 포인트: %d점", volumeCredit));
+        return result.toString();
+    }
 }
