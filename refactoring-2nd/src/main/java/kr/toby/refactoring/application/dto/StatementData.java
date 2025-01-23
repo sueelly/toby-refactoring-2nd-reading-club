e491b633e8215f9b6ccc74dbf24b65fe6d7fe5c7
김혜주 <sueelly@naver.com>
2025-01-23 18:47:41 +0900

    refactor(PerformanceCalculator): Extract calculate by performance logic to PerforamceCalculator class

--- refactoring-2nd/src/main/java/kr/toby/refactoring/application/dto/StatementData.java
+++ refactoring-2nd/src/main/java/kr/toby/refactoring/application/dto/StatementData.java
@@ -5,12 +5,12 @@ import java.util.List;
 import lombok.AllArgsConstructor;
 
 import kr.toby.refactoring.common.exception.RefactoringException;
-import kr.toby.refactoring.common.exception.ResultCode;
 import kr.toby.refactoring.domain.invoice.Invoice;
 import kr.toby.refactoring.domain.invoice.Performance;
 import kr.toby.refactoring.domain.play.Play;
 import kr.toby.refactoring.domain.play.Plays;
 import kr.toby.refactoring.domain.play.enums.Type;
+import kr.toby.refactoring.application.PerformanceCalculator;
 
 @AllArgsConstructor
 public class StatementData {
@@ -26,45 +26,18 @@ public class StatementData {
         return invoice.getCustomer();
     }
 
-    public int amountFor(Performance performance) 
-            throws RefactoringException {
-        int result;
-
-        switch (playFor(performance).getType()) {
-            case TRAGEDY:
-                result = 40000;
-                if (performance.getAudience() > 30) {
-                    result += 1000 * (performance.getAudience() - 30);
-                }
-                break;
-            case COMEDY:
-                result = 30000;
-                if (performance.getAudience() > 20) {
-                    result += 10000 + 500 * (performance.getAudience() - 20);
-                }
-                result += 300 * performance.getAudience();
-                break;
-            default:
-                throw new RefactoringException(ResultCode.INVALID_PLAY_TYPE);
-        }
-        return result;
-    }
-
     public Play playFor(Performance performance) {
         return plays.get(performance);
     }
 
+    public int amountFor(Performance performance) throws RefactoringException {
+        return new PerformanceCalculator(performance, playFor(performance))
+                .amountFor();
+    }
+
     public int volumeCreditFor(Performance performance) {
-        int result = 0;
-
-        // Add volume credits for each performance
-        result += Math.max(performance.getAudience() - 30, 0);
-
-        // Add extra volume credits for every 5 comedy attendees
-        if (playFor(performance).getType() == Type.COMEDY) {
-            result += Math.floor(performance.getAudience() / 5);
-        }
-        return result;
+        return new PerformanceCalculator(performance, playFor(performance))
+                .volumeCreditFor();
     }
 
     public int totalAmount() throws RefactoringException {
